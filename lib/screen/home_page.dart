cc1d8cdd81312f08d708bc3afee488c1bebada47
devsoupe <luvyum4e@gmail.com>
2022-09-16 23:16:47 +0900

    feat: 각 State에 맞게 build 메소드 분리, Loading State 구현

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_movie_list/repository/repository.dart';
 import 'package:flutter_movie_list/screen/home_bloc.dart';
 
 part 'widget/cover_carousel.dart';
+
 part 'widget/poster_carousel.dart';
 
 enum Section {
@@ -22,7 +23,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   void initState() {
     final bloc = context.read<HomeBloc>();
@@ -36,65 +36,90 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: MediaQuery.removePadding(
-        context: context,
-        removeTop: true,
-        child: ListView.separated(
-          itemBuilder: (BuildContext context, int index) {
-            switch (Section.values[index]) {
-              case Section.nowPlaying:
-                return _CoverCarouselWidget();
-              case Section.popular:
-                return _PosterCarouselWidget(
-                  type: PosterType.popular,
-                );
-              case Section.upcoming:
-                return _PosterCarouselWidget(
-                  type: PosterType.upcoming,
-                );
-              default:
-                throw UnimplementedError();
-            }
-          },
-          itemCount: Section.values.length,
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider();
-          },
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.black,
-        child: IconTheme(
-          data: IconThemeData(color: Colors.white),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                icon: Icon(
-                  Icons.home,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.favorite_outline,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.person_outline),
-                onPressed: () {},
-              ),
-            ],
+    return BlocConsumer<HomeBloc, HomeState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.black,
+          body: MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: _buildBody(context, state),
           ),
-        ),
-      ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.black,
+            child: IconTheme(
+              data: const IconThemeData(color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.home,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.favorite_outline,
+                    ),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.person_outline),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildBody(BuildContext context, HomeState state) {
+    final stateType = state.runtimeType;
+    switch (stateType) {
+      case Loading:
+        return _buildLoading(context);
+      case Loaded:
+        return _buildLoaded(context);
+      default:
+        return const SizedBox();
+    }
+  }
+
+  Widget _buildLoading(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget _buildLoaded(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        switch (Section.values[index]) {
+          case Section.nowPlaying:
+            return const _CoverCarouselWidget();
+          case Section.popular:
+            return const _PosterCarouselWidget(
+              type: PosterType.popular,
+            );
+          case Section.upcoming:
+            return const _PosterCarouselWidget(
+              type: PosterType.upcoming,
+            );
+          default:
+            throw UnimplementedError();
+        }
+      },
+      itemCount: Section.values.length,
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider();
+      },
     );
   }
 }
