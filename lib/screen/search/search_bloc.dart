e9e7115b9ad193cc58d6865505b33dce54c73fb2
devsoupe <luvyum4e@gmail.com>
2022-09-20 23:50:19 +0900

    feat: Search BLoC 추가

---
+++ lib/screen/search/search_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'search_event.dart';
+part 'search_state.dart';
+
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  SearchBloc() : super(SearchInitial()) {
+    on<SearchEvent>((event, emit) {
+    });
+  }
+}
