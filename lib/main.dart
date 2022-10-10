c5b1cd51ed3b50c952f3afa656d893fb3c72bd1d
tidyline <tidyaline@gmail.com>
2022-10-10 15:43:05 +0900

    :recycle: Repository Provider 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie_list/api/api_provider.dart';
+import 'package:flutter_movie_list/repository/repository.dart';
 import 'package:flutter_movie_list/screen/home.dart';
 
 void main() {
@@ -13,13 +16,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return RepositoryProvider<MovieRepository>(
+      create: (context) => MovieRepository(apiProvider: ApiProviderImpl()),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
