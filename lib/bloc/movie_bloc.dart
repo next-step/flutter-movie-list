447ce7c65adda56cc58f7e04c03b4e667e5e6d29
tidyline <tidyaline@gmail.com>
2022-10-10 16:10:45 +0900

    :sparkles: ApiState, MovieBloc 생성

---
+++ lib/bloc/movie_bloc.dart
@@ -0,0 +1,58 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_movie_list/bloc/src/api_state.dart';
+import 'package:flutter_movie_list/model/model.dart';
+import 'package:flutter_movie_list/repository/repository.dart';
+
+abstract class MovieCubit extends Cubit<ApiState> {
+  MovieCubit() : super(LoadingState());
+
+  void load();
+}
+
+class UpcomingMovieCubit extends MovieCubit {
+  UpcomingMovieCubit({required this.repository});
+
+  final MovieRepository repository;
+
+  @override
+  void load() async {
+    try {
+      final movieList = await repository.getUpcoming();
+      emit(LoadedState<MovieList>(data: movieList));
+    } catch (e) {
+      emit(LoadErrorState());
+    }
+  }
+}
+
+class PopularMovieCubit extends MovieCubit {
+  PopularMovieCubit({required this.repository});
+
+  final MovieRepository repository;
+
+  @override
+  void load() async {
+    try {
+      final movieList = await repository.getPopular();
+      emit(LoadedState<MovieList>(data: movieList));
+    } catch (e) {
+      emit(LoadErrorState());
+    }
+  }
+}
+
+class NowPlayingMovieCubit extends MovieCubit {
+  NowPlayingMovieCubit({required this.repository});
+
+  final MovieRepository repository;
+
+  @override
+  void load() async {
+    try {
+      final movieList = await repository.getNowPlaying();
+      emit(LoadedState<MovieList>(data: movieList));
+    } catch (e) {
+      emit(LoadErrorState());
+    }
+  }
+}
