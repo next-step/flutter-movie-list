db9737ff551f83d1e67b7cb6aa8e239874ae4cf6
malibinYun <nightmare73@naver.com>
2022-11-20 17:41:24 +0900

    [add] movie bloc을 poster carousel에 연결

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_list/api/api_provider.dart';
+import 'package:flutter_movie_list/bloc/movies_bloc.dart';
 import 'package:flutter_movie_list/model/model.dart';
 import 'package:flutter_movie_list/repository/repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'widget/cover_carousel.dart';
 part 'widget/poster_carousel.dart';
