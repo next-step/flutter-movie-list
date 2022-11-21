5ab1e899ae2a2b3d65f2240f9ff77e28a7941a63
teahyuk <jtaeh57@gmail.com>
2022-11-21 20:59:23 +0900

    detail view 개발

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -5,21 +5,74 @@ class Detail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // 가로축 가운데 정렬
-      child: Column(
-        // 세로 컬럼 생성
-        mainAxisAlignment: MainAxisAlignment.center, // 새로축 가운데 정렬
-        children: const [
-          Text(
-            'detail page',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(0.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              'https://image.tmdb.org/t/p/original/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg',
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              'movie.title',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: const [
+                Text(
+                  '★',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '별점  날짜',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              'detailTextdetailTextdetailTextdetailTextdetailTextdetailTextdetailText'
+                  'detailTextdetailTextdetailTextdetailTextdetailTextdetailText'
+                  'detailTextdetailTextdetailTextdetailTextdetailTextdetailText'
+                  'detailTextdetailTextdetailTextdetailTextdetailTextdetailText',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
