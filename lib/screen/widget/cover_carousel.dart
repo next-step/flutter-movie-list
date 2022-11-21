8fe4e08feb255fd73243544bbb9054489ef43229
teahyuk <jtaeh57@gmail.com>
2022-11-21 20:19:40 +0900

    cover carousel 코드클린

--- lib/screen/widget/cover_carousel.dart
+++ lib/screen/widget/cover_carousel.dart
@@ -105,33 +105,34 @@ class _CarouselTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => const Detail())),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://image.tmdb.org/t/p/original/${movie.backdropPath}'),
-              fit: BoxFit.cover,
-            ),
+      onTap: () => Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const Detail())),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://image.tmdb.org/t/p/original/${movie.backdropPath}'),
+            fit: BoxFit.cover,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                movie.title,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              movie.title,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
