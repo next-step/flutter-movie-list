516182aa9d1aa9e0b765ca0889efce9432bd391b
teahyuk <jtaeh57@gmail.com>
2022-11-22 21:07:17 +0900

    Terminal 2단계코드리뷰 반영 2: detail widget 쪼개기

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -4,6 +4,10 @@ import 'package:flutter_movie_list/bloc/movie_detail_bloc.dart';
 
 import '../repository/src/movie_repository.dart';
 
+part 'widget/detail_title.dart';
+part 'widget/detail_overview.dart';
+part 'widget/detail_start_and_release_date.dart';
+
 class Detail extends StatelessWidget {
   final int id;
 
@@ -31,93 +35,60 @@ class Detail extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              BlocBuilder<MovieDetailBloc, MovieDetailState>(
-                builder: (context, state) {
-                  if (state.detail?.backdropPath != null) {
-                    return Image.network(
-                      'https://image.tmdb.org/t/p/original${state.detail?.backdropPath ?? ''}',
-                      loadingBuilder: (context, child, loadingProgress) {
-                        if (loadingProgress == null) {
-                          return child;
-                        }
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 100,
-                          ),
-                          child: Center(
-                            child: CircularProgressIndicator(
-                              value: loadingProgress.expectedTotalBytes != null
-                                  ? loadingProgress.cumulativeBytesLoaded /
-                                      loadingProgress.expectedTotalBytes!
-                                  : null,
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }
-                  return const SizedBox();
-                },
-              ),
-              const SizedBox(
+            children: const [
+              _DetailImage(),
+              SizedBox(
                 height: 40,
               ),
-              BlocBuilder<MovieDetailBloc, MovieDetailState>(
-                builder: (context, state) => Text(
-                  state.detail?.title ?? '',
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(
+              _DetailTitle(),
+              SizedBox(
                 height: 20,
               ),
-              Row(
-                children: [
-                  const Text(
-                    '★',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  BlocBuilder<MovieDetailBloc, MovieDetailState>(
-                    builder: (context, state) => Text(
-                      '${state.detail?.voteAverage ?? 0.0} ${state.detail?.releaseDate ?? ''}',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
+              _DetailStartAndReleaseDate(),
+              SizedBox(
                 height: 5,
               ),
-              BlocBuilder<MovieDetailBloc, MovieDetailState>(
-                builder: (context, state) => Text(
-                  state.detail?.overview ?? '',
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              _DetailOverview(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _DetailImage extends StatelessWidget {
+  const _DetailImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MovieDetailBloc, MovieDetailState>(
+      builder: (context, state) {
+        if (state.detail?.backdropPath != null) {
+          return Image.network(
+            'https://image.tmdb.org/t/p/original${state.detail?.backdropPath ?? ''}',
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 100,
+                ),
+                child: Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                        : null,
+                  ),
+                ),
+              );
+            },
+          );
+        }
+        return const SizedBox();
+      },
     );
   }
 }
